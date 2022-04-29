2247d09d052b16f99303f836ae2bc5d65d5db2d3
zhsaule <zhsaule@gmail.com>
2022-04-30 01:19:10 +0600

    Fix logger.py

--- logger.py
+++ logger.py
@@ -6,13 +6,9 @@
 import os
 from datetime import datetime
 
-""" 1. Декоратор - логгер. Запись ведется в файл logger.txt в текущий каталог.
-    type = 'a' - Открывает файл для добавления. Указатель файла находится в конце файла,
-    если файл существует. То есть файл находится в режиме добавления. Если файл не существует,
-    он создает новый файл для записи.   """
-
 
 def logger(old_function):
+    """ 1. Декоратор - логгер. Запись ведется в файл logger.txt в текущий каталог."""
     file_path = os.path.join(os.getcwd(), 'logger.txt')
     print(f'Start logger: {datetime.now()} {file_path} Func: {old_function.__name__}')
 
@@ -26,16 +22,10 @@ def logger(old_function):
     return new_function
 
 
-""" 2. Параметризированный декоратор, с параметром – путь к логам и типом логирования.
-    type = 'a' - Открывает файл для добавления. Указатель файла находится в конце файла,
-    если файл существует. То есть файл находится в режиме добавления. Если файл не существует,
-    он создает новый файл для записи. """
-
-
 def path_logger(_file_path):
+    """ 2. Параметризированный декоратор, с параметром – путь к логам."""
     def _logger(old_function):
         file_path = os.path.join(_file_path, 'logger_path.txt')
-
         print(f'Start logger: {datetime.now()} {file_path} Func: {old_function.__name__}')
 
         def new_function(*args, **kwargs):
